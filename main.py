ad3fff0801bcae48ad42ee15b8bd82478c51bb35
Lee-seunghyeon2 <james0107james@gmail.com>
2024-11-06 21:26:05 +0900

    Implement even_list function

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def even_list(int_list: List[int]) -> List[int]:
     """    Determines if a number is even and return an even list.    Args:        int_list: A list of integer.    Returns:        A list of even integers.    """    
 
     # TODO: Implement even_list
+    even = []
+    for num in int_list:
+        if num%2==0:
+            even.append(num)
     
+    return even
     pass
 
 # Skeleton code for sum_of_squares_of_even
@@ -13,7 +18,8 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     """    Computes the sum of the squares of all even numbers in a l    Args:        even_int_list: A list of even integers.    Returns:        The sum of the squares of all even numbers in the lisAssignment3    """    
 
     # TODO: Implement sum_of_squares_of_even
-    
+
+
     pass
 
 # Main function
